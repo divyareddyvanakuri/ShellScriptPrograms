8d0a48b8bc5867c5ca5421ee73d1b95b1d7e675c
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-12 12:12:54 +0530

    [Divya].Refactor: Added the comments to script of distance calcualation and indentation

--- FunctionalPrograms/distancecal.sh
+++ FunctionalPrograms/distancecal.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#A simple script for distance calculation from points(x,y) to origin
+
+#reading the points(x,y) variables
 read -p "enter x value:" x
 read -p "enter y value:" y
+#equation for distance calculation
 c=$(($((x * x))+$((y * y))))
+#to disaly decimal values also
 d=$(echo "scale=2;sqrt($c)" | bc)
+#print distance
 echo "$d"
