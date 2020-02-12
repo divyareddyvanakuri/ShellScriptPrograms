194dd810ced38f4bd43264bf7f40595b8b28aad7
divyareddyvanakuri <divyavanakuri48@gmail.com>
2020-02-12 11:46:55 +0530

    [Divya].Refactor: Added the comments to script of 2d array and indentation

--- FunctionalPrograms/2darray.sh
+++ FunctionalPrograms/2darray.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
+#A simple script for  2D array to read and display
+
+#reading variables the number of columns and rows
 read -p "enter number of columns:" m
 read -p "enter number of rows:" n
+#to create an array using  in-built function "declare"
 declare -A array
+#loop for to add values to the array
+#loop for columns
 for (( i = 0; i < m; i++ )); do
+	#loop for rows
 	for (( j = 0; j < n; j++ )); do
+		#read the argumnets
 		read -p "enter value: " number
 		array[$i,$j]=$number
 	done
 done
+#to display the arguments inside an array
 for (( i = 0; i < m; i++ )); do
 	for (( j = 0; j < n; j++ )); do
+		#print
 		echo "${array[$i,$j]}"
 	done
 done
